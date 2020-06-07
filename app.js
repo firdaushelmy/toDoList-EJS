be5effe6c3826560ed4d99c6f5d9e9654f88e613
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-07 23:43:20 +0800

    set up ejs

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs')
+
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('initial page test');
-})
+  const today = new Date();
+  var day = '';
+
+  if (today.getDay() === 6) {
+    res.send('Yayy its the weekend');
+  } else {
+    res.send('i have to work today');
+  }
+});
 
 const port = process.env.PORT || 3000;
 
