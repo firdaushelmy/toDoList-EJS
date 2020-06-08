045a7a60e7fc617d1005a97ac8b8eb9fbef7123e
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-08 21:28:51 +0800

    add form in list.ejs + add app.post to push items into list.ejs to rez new submitted <li>

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const items = [];
 
 app.get('/', function (req, res) {
   const day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const today = new Date();
   let callingDay = day[today.getDay()]
 
-  res.render('list', { kindOfDay: callingDay });
+  res.render('list', { kindOfDay: callingDay, newListItems: items });
 })
+
+app.post('/', function (req, res) {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect('/');
+});
 
 const port = process.env.PORT || 3000;
 
