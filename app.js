37033272d77a062fd970235f85fffc31032c5d7d
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-18 04:22:21 +0800

    add schema, model, and document insertion

--- app.js
+++ app.js
@@ -10,6 +10,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 mongoose.connect('mongodb://localhost/27017/todolistDB', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const itemsSchema = ({
+  task: {
+    type: 'string',
+    required: true
+  }
+}),
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item = new Item({
+  task: 'do laundry'
+})
+
+const item = new Item({
+  task: 'Hit the + button to add task'
+})
+
+const item = new Item({
+  task: '<---- hit this to delete a task'
+})
+
 // const items = [];
 // const workItems = [];
 
