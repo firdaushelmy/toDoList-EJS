e36dc3b27afbd29e9efb9e8855cd90cbc0d8bdb2
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-24 22:59:11 +0800

    connect to mongoDB atlas

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js');
 const _ = require('lodash');
 const app = express();
+require('dotenv').config()
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/todolistDB', { useNewUrlParser: true, useUnifiedTopology: true })
+const MONGO_URL = process.env.MONGO_URL;
+mongoose.connect(`${MONGO_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const itemsSchema = ({
   task: {
